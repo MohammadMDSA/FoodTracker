13423779bf43a73156499b75a34428a84f976bc8
Mohammad Sami <samimd.77@gmail.com>
2018-06-16 02:44:25 -0700

    Added Ration control

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,32 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Mohammad Sami on 6/16/18.
+//  Copyright © 2018 MS. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    // MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+        
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder);ç
+    }
+
+}
